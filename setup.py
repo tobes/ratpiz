93300acce6c1042a3b0ca8940f5e474e44bc0929
Toby Dacre <toby.junk@gmail.com>
2018-08-19 20:51:13 +0100

    add entry points

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     license='GPLv2',
     packages=['ratpiz'],
     zip_safe=False,
+    entry_points={
+    'console_scripts': [
+        'ratpiz = ratpiz.process_runner:main',
+        'ratpiz-register = ratpiz.process_runner:register',
+    ]
+},
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX :: Linux',
